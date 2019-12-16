8e063083f4da12c970a07dabf96ad05eb110a0b4
kajal-patel97 <57909923+kajal-patel97@users.noreply.github.com>
2019-12-16 16:15:32 +0000

    long and lat fucntion added

--- task_fucntions.py
+++ task_fucntions.py
@@ -28,4 +28,23 @@ def dictionary_postcode():
     return 'Completed'
 
 
-print(dictionary_postcode())
+
+def get_long_lat():
+    ask_postcode = input('Please enter a postcode...')
+
+    # Make a dictionary
+    dictionary_pc = {'postcodes': [ask_postcode]}
+
+    json_body = json.dumps(dictionary_pc)
+
+# build a URL
+    base_url = 'http://api.postcodes.io/'
+    path = 'postcodes/'
+    headers = {'Content-type': 'application/json'}
+
+    long_lat_response = requests.post(base_url + path, data=json_body, headers=headers)
+
+    results = long_lat_response.json()['result']
+    for request in results:
+        print(f"Longitude: {request['result']['longitude']}. Latitude: {request['result']['latitude']}")
+
